0e344e2fe54a8f8c5e2c0dae193d76decb3a0d38
Natalia Lopardo <lailatan@gmail.com>
2022-03-10 09:47:13 -0300

    Agrego botones login/edicion

--- MockUp/js/main.js
+++ MockUp/js/main.js
@@ -1,19 +1,31 @@
-window.onload = function(){
-    var barraCarga = document.getElementById('barra-carga');
-    
-    barraCarga.style.visibility = 'hidden';
-    barraCarga.style.display = 'none';
-}
+//window.onload = function(){
+//    var boton = document.getElementById('boton-arriba');
+//    if( window.scrollTop() > 0 ){
+//      boton.style.display = 'none';
+//    }
+//}
+
 $('document').ready(function(){
 
-    var boton = document.getElementById('boton');
+    //var boton = document.getElementById('boton');
         
     $('#boton-arriba').click(function(){
         $('body, html').animate({
           scrollTop: '0px'
-        }, 300);
+        }, 300);       
       });
-   
+
+    $('#boton-login').click(function(){
+      var all = document.getElementsByClassName("boton-edicion");
+      for (var i=0, max=all.length; i < max; i++) {
+        all.item(i).style.display="inline-block";  
+      }
+      var all_reverse = document.getElementsByClassName("boton-edicion-reverse");
+      for (var i=0, max=all_reverse.length; i < max; i++) {
+        all_reverse.item(i).style.display="inline-block";  
+      }
+  });
+
     $(window).scroll(function(){
       if( $(this).scrollTop() > 0 ){
         $('#boton-arriba').slideDown(300);
